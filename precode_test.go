d248e808a17a76c507f1ff448af9d88ca1fdf42d
Ismail <smlkorigov@gmail.com>
2024-04-06 15:24:17 +0300

    SecondUpload

--- precode_test.go
+++ precode_test.go
@@ -21,33 +21,32 @@ func TestMainHandlerWheOK(t *testing.T) {
 	response := responseRecorder.Result()
 
 	//проверка возврата код ответа - 200
-	require.Equal(t, 200, response.StatusCode, "Unexpected status code")
+	assert.Equal(t, 200, response.StatusCode, "Unexpected status code")
 	//тело ответа не пустое
-	require.NotEmpty(t, response.Body, "Response body is empty")
+	assert.NotEmpty(t, response.Body, "Response body is empty")
 
-	// здесь нужно добавить необходимые проверки
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
 	response := responseRecorder.Result()
+	require.Equal(t, 200, response.StatusCode, "Unexpected status code")
 
 	body, _ := io.ReadAll(response.Body)
 	list := strings.Split(string(body), ",")
 
 	assert.Len(t, list, totalCount, "wrong count value")
 
-	// здесь нужно добавить необходимые проверки
 }
 
 func TestMainHandlerWhenWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=TestingCity", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=TestingCity", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
@@ -60,5 +59,4 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 	//проверяем что тело ответа содержитт сообщение об ошибке
 	require.Contains(t, string(body), "wrong city value")
 
-	// здесь нужно добавить необходимые проверки
 }
